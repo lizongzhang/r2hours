d16de04d3ee210f36446e7644da77b094bfcff1c
Your Name <zongzhangli@gmail.com>
2026-05-11 18:44:49 +0800

    1844

--- draft.R
+++ draft.R
@@ -141,3 +141,171 @@ clean_trial %>%
   
   # 5. 跨界融合：使用 add() 将 ggpubr 的 ANOVA 统计量图层叠加上去
   add(stat_anova_test(label.y.npc = "top", vjust = 1))
+
+
+# plot --------------------------------------------------------------------
+
+data(mtcars)
+
+plot(mtcars)
+
+plot(mtcars$wt, mtcars$mpg,
+     col = "lightblue", 
+     pch = 19,
+     cex = 1.5,
+     main = "Weight & MPG",
+     xlab = "Weight (1000 lbs)",
+     ylab = "Miles/(US) gallon"
+     )
+
+plot(mtcars$cyl, mtcars$mpg)
+
+plot(as.factor(mtcars$cyl), mtcars$mpg)
+
+plot(mtcars$mpg, type = "h")
+
+plot(as.factor(mtcars$cyl), mtcars$mpg,
+     col = c("#8DA0CB", "#FC8D62", "#66C2A5"),
+     border = "gray40",
+     las = 1,
+     main = "MPG Distribution by Engine Cylinders",
+     xlab = "Number of Cylinders",
+     ylab = "Miles/(US) gallon",
+     font.main = 2
+)
+
+plot(as.factor(mtcars$cyl), 
+     col = c("#8DA0CB", "#FC8D62", "#66C2A5"), 
+     border = "gray50",                     # 边框微调为中灰，更柔和
+     las = 1,                               # 刻度数字水平
+     main = "Distribution of Cylinders",    # 标题
+     xlab = "Number of Cylinders",          # X轴标签
+     font.main = 2,                         # 标题加粗
+     ylim = c(0, 15)                        #调整Y轴刻度范围
+)
+
+plot(as.factor(mtcars$vs),
+     col = c("skyblue", "lightgreen"),
+     border = "gray40",
+     las = 1,
+     main = "Engine",
+     xlab = "Engine (0 = V-shaped, 1 = straight)",
+     font.main = 2
+)
+
+model1 <- lm(mpg ~ wt + cyl + disp, data = mtcars)
+
+plot(model1)
+
+# 绘制正弦函数曲线，指定范围从 -2π 到 2π
+plot(sin, -2*pi, 2*pi, 
+     col = "skyblue", 
+     lwd = 2, 
+     main = "Sine Wave Form")
+
+
+# L4 全链路 ------------------------------------------------------------------
+
+data(mtcars)
+summary(mtcars)
+
+sapply(mtcars, mean)
+
+sapply(mtcars, 
+       function(x) c(Mean = mean(x), Sd = sd(x), Median = median(x), Min = min(x), Max = max(x)))
+
+
+hist(mtcars$mpg, 
+     col = "lightblue", 
+     border = "white", 
+     main = "Distribution of Miles Per Gallon",
+     xlab = "Miles/(US) gallon",
+     ylab = "Frequency",
+     ylim = c(0, 14),
+     las = 1)
+
+table(mtcars$cyl, mtcars$am)
+
+plot(as.factor(mtcars$cyl), as.factor(mtcars$am), 
+     col = c("#8DA0CB", "#FC8D62"), 
+     main = "Cylinders vs Transmission",
+     xlab = "Cylinders", 
+     ylab = "Transmission (0=Auto, 1=Manual)")
+
+
+plot(as.factor(mtcars$am), mtcars$mpg,
+     col = c("lightblue", "salmon"),
+     cex = 1.5,
+     main = "Transmission Type & MPG",
+     xlab = "Transmission (0 = automatic, 1 = manual)",
+     ylab = "Miles/(US) gallon",
+     las = 1
+)
+
+
+
+
+plot(as.factor(mtcars$cyl), mtcars$mpg,
+     col = c("#8DA0CB", "#FC8D62", "#66C2A5"), 
+     cex = 1.5,
+     main = "MPG by Number of Cylinders",
+     xlab = "Number of Cylinders",
+     ylab = "Miles/(US) gallon",
+     las = 1,
+     horizontal = TRUE
+)
+
+fisher.test(table(mtcars$cyl, mtcars$am))
+
+t.test(mtcars$mpg ~ mtcars$am)
+
+aov_result <- aov(mpg ~ as.factor(cyl), data = mtcars)
+summary(aov_result)
+
+
+plot(mtcars$wt, mtcars$mpg,
+     col = "lightblue", 
+     pch = 19,
+     cex = 1.5,
+     main = "Weight & MPG",
+     xlab = "Weight (1000 lbs)",
+     ylab = "Miles/(US) gallon"
+)
+
+model1 <- lm(mpg ~ wt + cyl + am, data = mtcars)
+summary(model1)
+
+
+
+# L5 help -----------------------------------------------------------------
+
+# 精准查询
+?hist
+
+# 模糊查询
+??hist
+
+example(hist)
+
+mean
+
+methods("plot")
+
+
+methods(summary)
+
+
+
+# L8 ----------------------------------------------------------------------
+
+# 错误：col 参数识别不了这种散装写法
+plot(1:3, col = "red", "blue", "green") 
+
+# 正确：必须打包成向量
+plot(1:3, col = c("red", "blue", "green"))
+
+
+x <- 1
+
+x %in% 1, 2, 3 # 这实际上是 (x %in% 1), 2, 3，逻辑不对
+x %in% c(1, 2, 3)
