710a28b4373bf38eb3dbb5f6616328950c6df861
Your Name <zongzhangli@gmail.com>
2026-05-14 07:03:34 +0800

    0703

--- at class.R
+++ at class.R
@@ -1,43 +1,16 @@
 
+?plot()
 
-# L3 ----------------------------------------------------------------------
+data(mtcars)
 
-## L3.1 ----------------------------------------------------------------------
+summary()
 
+lm(mpg ~ wt, data = mtcars)
 
-# 四则运算
-8*9
+hist(mtcars$mpg,
+     freq = FALSE, 
+     breaks = seq(10, 40, 10),
+     col = "cyan",
+     border = "white")
 
-## L3.2 ----------------------------------------------------------------------
-
-
-
-
-# L4 ----------------------------------------------------------------------
-
-data("mtcars")
-
-summary(mtcars)
-
-# <- alt + -; option + -
-mpg_mean <- mean(mtcars$mpg)
-
-View(mtcars)
-
-# data frame
-plot(mtcars)
-
-plot(mtcars$wt, mtcars$mpg,
-     col = "blue",
-     cex = 1.5,
-     main = "weight & MPG",
-     xlab = "weight",
-     ylab = "Miles per Gallon")
-
-plot(as.factor(mtcars$cyl), mtcars$mpg,
-     col = c("red","blue", "green"))
-
-plot(as.factor(mtcars$cyl))
-
-
-
+corrplot(cor(mtcars))
